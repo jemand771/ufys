3ad56a040bfb670bac63e2dee94c640da6e3ddd8
Willy <jemand771@gmx.net>
2023-07-02 01:45:44 +0200

    test video is gone (again), replace with new one

--- test/constants.py
+++ test/constants.py
@@ -1,3 +1,3 @@
 video_not_found = "https://vm.tiktok.com/ZMNCLNbUT/"
-video_direct_linkable = "https://vm.tiktok.com/ZMFw4hrwm/"
+video_direct_linkable = "https://www.tiktok.com/@zentonte/video/7247972994759413019"
 video_needs_reupload = "https://www.reddit.com/r/196/comments/100dqbh/meta_interview_rule/"
